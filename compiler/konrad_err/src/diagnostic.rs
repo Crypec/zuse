c5138a454e41253dc50171e70cf2cf22fff195a8
Simon Kunz <simon.kunz21@gmail.com>
2021-04-25 23:20:32 +0200

    Feat: Add support for diagnostics
    A `Diagnostic` represents the fundamental data structure the compiler
    uses to interface with the user. Depending on the use case it can
    represent an error, warning, a note or an interal bug in the compiler.

---
+++ compiler/konrad_err/src/diagnostic.rs
@@ -0,0 +1,43 @@
+use konrad_span::span::*;
+
+use typed_builder::TypedBuilder;
+
+/// A Diagnostic represents a message to the user
+#[derive(Debug, TypedBuilder)]
+pub struct Diagnostic {
+    pub lvl: Level,
+    pub span: Span,
+
+    #[builder(setter(into))]
+    pub msg: String,
+
+    #[builder(default, setter(strip_option, into))]
+    pub note: Option<String>,
+
+    #[builder(default)]
+    pub suggestion: Vec<Suggestion>,
+}
+
+#[derive(Debug)]
+/// Represents the kind of diagnostic the compiler is supposed to emmit
+pub enum Level {
+    /// Hard compile time errors
+    Error,
+    /// Soft compile time warnings. The compiler will still exit with sucessfully even if you emit
+    /// a Warning.
+    Warning,
+
+    /// A Note can be used to give extra information to the user.
+    Note,
+
+    /// Internal bugs inside the compiler. The end user is never supposed to see this.
+    Internal,
+}
+
+#[derive(Debug, TypedBuilder)]
+pub struct Suggestion {
+    #[builder(setter(into))]
+    pub msg: String,
+    #[builder(setter(into))]
+    pub url: String,
+}
