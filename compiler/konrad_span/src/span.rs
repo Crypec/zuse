e8f3e70a9c406c759802296b4ff5e729fe73c1c2
Simon Kunz <simon.kunz21@gmail.com>
2021-04-26 20:24:04 +0200

    Refactor: Make working with spans easier
    This change is supposed to make working with spans a little bit easier. That
    being said it is not unlikely that this system is going to change in the
    future. Considering the number of spans compiling even a small programm
    is going to involve, means that reducing the size of a `Span` is probably
    going to provide a sizeable benefit in both speed and memory footprint.
    
    We could probably get away with storing just the start and end offset into the
    the source string and keep a global copy of the file path somewhere in
    the AST.
    
    But right now performance and memory footprint are smaller concerns than
    getting an initial version of this language up and running. So at least
    for now we're not going to think too much about these concerns :D

--- compiler/konrad_span/src/span.rs
+++ compiler/konrad_span/src/span.rs
@@ -2,29 +2,52 @@ use std::fmt;
 use std::fmt::{Debug, Formatter};
 use std::path::PathBuf;
 
+#[derive(Clone, PartialEq, Eq)]
+pub struct LineColumn {
+    pub line: usize,
+    pub col: usize,
+}
+
+impl LineColumn {
+    pub fn new(line: usize, col: usize) -> Self {
+        Self { line, col }
+    }
+}
+
 /// Span is used to store the src location of nodes in the compiler
-#[derive(Clone)]
+#[derive(Clone, Eq, PartialEq)]
 pub struct Span {
-    pub lo: usize,
-    pub hi: usize,
+    pub start: LineColumn,
+    pub end: LineColumn,
     pub path: PathBuf,
 }
 
 impl Span {
-    pub fn new<P: Into<PathBuf>>(lo: usize, hi: usize, path: P) -> Self {
+    pub fn new<P: Into<PathBuf>>(start: LineColumn, end: LineColumn, path: P) -> Self {
         debug_assert!(
-            lo <= hi,
-            "Low must be smaller or equal to high {}..{}",
-            lo,
-            hi
+            start.line <= end.line,
+            "start must be smaller or equal to end start: {} :: end: {}",
+            start.line,
+            end.line,
         );
         let path = path.into();
-        Self { lo, hi, path }
+        Self { start, end, path }
+    }
     }
 }
 
 impl Debug for Span {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        write!(f, "[{:?}] :: {}..{}", self.path, self.lo, self.hi)
+        // NOTE(Simon): We use zero indexing for the line numbers so we need to add one
+        // NOTE(Simon): show the correct line number!
+        write!(
+            f,
+            "[{:?}] ----> [{} :: {}]..[{} :: {}]",
+            self.path,
+            self.start.line + 1,
+            self.start.col,
+            self.end.line + 1,
+            self.end.col
+        )
     }
 }
