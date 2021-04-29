c9486a55d93d321b3c02810332968c4eedb582e7
Simon Kunz <simon.kunz21@gmail.com>
2021-04-29 22:49:03 +0200

    Refactor: Make span API more ergonomic
    By implementing PartialOrd + Ord for LineColumn we don't need to worry
    about the order of the LineColumns and therefore prevent the creation of
    "logically malformed" spans where start > end.

--- compiler/konrad_span/src/span.rs
+++ compiler/konrad_span/src/span.rs
@@ -1,8 +1,9 @@
+use std::cmp::*;
 use std::fmt;
 use std::fmt::{Debug, Formatter};
 use std::path::PathBuf;
 
-#[derive(Clone, PartialEq, Eq)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub struct LineColumn {
     pub line: usize,
     pub col: usize,
@@ -15,6 +16,7 @@ impl LineColumn {
 }
 
 /// Span is used to store the src location of nodes in the compiler
+/// It is treated like an inclusive range!
 #[derive(Clone, Eq, PartialEq)]
 pub struct Span {
     pub start: LineColumn,
@@ -23,19 +25,19 @@ pub struct Span {
 }
 
 impl Span {
-    pub fn new<P: Into<PathBuf>>(start: LineColumn, end: LineColumn, path: P) -> Self {
-        debug_assert!(
-            start.line <= end.line,
-            "start must be smaller or equal to end start: {} :: end: {}",
-            start.line,
-            end.line,
-        );
+    pub fn new<P: Into<PathBuf>>(lhs: LineColumn, rhs: LineColumn, path: P) -> Self {
+        let start = std::cmp::min(lhs, rhs);
+        let end = std::cmp::max(lhs, rhs);
         let path = path.into();
         Self { start, end, path }
     }
 
     fn merge(&self, other: &Self) -> Self {
-        debug_assert_eq!(self.path, other.path, "Paths must be equal");
+        debug_assert_eq!(
+            self.path, other.path,
+            "Paths must be equal: {:?} != {:?}",
+            self.path, other.path,
+        );
 
         let start_line = std::cmp::min(self.start.line, self.end.line);
         let end_line = std::cmp::max(self.start.line, self.end.line);
@@ -64,5 +66,20 @@ impl Debug for Span {
             self.end.line + 1,
             self.end.col
         )
+}
+
+impl Ord for LineColumn {
+    fn cmp(&self, other: &Self) -> Ordering {
+        if self.line != other.line {
+            self.line.cmp(&other.line)
+        } else {
+            self.col.cmp(&other.col)
+        }
+    }
+}
+
+impl PartialOrd for LineColumn {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(&other))
     }
 }
