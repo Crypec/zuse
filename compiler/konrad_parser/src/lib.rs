97c03bc6bb56726f2985f269fe763a499b0911d4
Simon Kunz <simon.kunz21@gmail.com>
2021-05-03 19:42:11 +0200

    Feat: Add parser
    The job of the parser is to take the stream of tokens provided by the
    lexer and parse them into an Abstract Syntax Tree (AST).
    The AST encapsulates much more information about the intend of the code
    compared to just a raw stream of tokens.
    
    Example:
    
    main :: () {
        foo := 42;
    }
    
    By purly looking at the tokens the lexer generates for the programm
    above we can make no definitive statement about the intend of the code.
    All we can see for certain are some some identifiers, numbers and a bunch
    of different delimiters.
    
    By feeding the tokens throuh the parser and looking at the generated
    AST we get a much more interesting picture which might look something like this:
    
    FnDecl {
        name: "main",
        body: Block {
    	VariableDefinition: {
    	    name: "foo",
    	    value: 42,
    	}
        }
        return_type: Void,
    }
    
    As you can see we now have a much greater understanding about the source
    programm.
    We know the user has written a function with the name "main" which does
    not return something and in this function he just creates a single
    variable "foo" and then does nothing with it.
    Seems like a pretty boring function if you ask me but what are supposed
    to do. It's not like we can change the users source code, can we? 😉

---
+++ compiler/konrad_parser/src/lib.rs
@@ -0,0 +1,6 @@
+#![feature(or_patterns)]
+#![feature(box_syntax)]
+#![warn(clippy::nursery)]
+#![warn(clippy::perf)]
+
+pub mod parser;
---
+++ compiler/konrad_parser/src/parser.rs
@@ -0,0 +1,143 @@
+use konrad_ast::ast::*;
+use konrad_err::diagnostic::*;
+use konrad_lexer::token::*;
+use konrad_span::span::*;
+use std::convert::TryInto;
+
+type PResult<T> = Result<T, Diagnostic>;
+
+#[derive(Debug)]
+pub struct Parser {
+    cursor: usize,
+    buf: Vec<Token>,
+}
+
+macro_rules! __parse_bin_expr_impl {
+    ($name:ident, $inner:ident, $cond:pat) => {
+        fn $name(&mut self) -> PResult<Expr> {
+            let mut lhs = self.$inner()?;
+            #[allow(unused_parens)]
+            while let $cond = self.peek()?.kind {
+                let op = self.next_t()?.try_into()?;
+                let rhs = self.$inner()?;
+                let span = lhs.span.merge(&rhs.span);
+                lhs = Expr {
+                    kind: ExprKind::Binary {
+                        lhs: box lhs,
+                        op,
+                        rhs: box rhs,
+                    },
+                    span,
+                };
+            }
+            Ok(lhs)
+        }
+    };
+}
+
+impl Parser {
+    pub fn new(buf: Vec<Token>) -> Self {
+        Self { cursor: 0, buf }
+    }
+
+    fn parse_expr(&mut self) -> PResult<Expr> {
+        todo!()
+    }
+
+    __parse_bin_expr_impl!(parse_or, parse_and, TokenKind::Or);
+    __parse_bin_expr_impl!(parse_and, parse_eq, TokenKind::And);
+    __parse_bin_expr_impl!(parse_eq, parse_cmp, (TokenKind::EqEq | TokenKind::NotEq));
+    __parse_bin_expr_impl!(
+        parse_cmp,
+        parse_term,
+        (TokenKind::Greater | TokenKind::GreaterEq | TokenKind::Less | TokenKind::LessEq)
+    );
+    __parse_bin_expr_impl!(
+        parse_term,
+        parse_factor,
+        (TokenKind::Plus | TokenKind::Minus)
+    );
+    __parse_bin_expr_impl!(
+        parse_factor,
+        parse_unary,
+        (TokenKind::Star | TokenKind::Slash)
+    );
+
+    fn parse_unary(&mut self) -> PResult<Expr> {
+        todo!()
+    }
+
+    fn eat(&mut self, expected: TokenKind, note: &str) -> PResult<&Token> {
+        match self.next_t() {
+            Ok(t) if t.kind == expected => Ok(t),
+            Ok(t) => {
+                let msg = format!(
+                    "Expected the following token: `{}` at this point instead of `{}`",
+                    expected, t.kind
+                );
+                let diag = Diagnostic::builder()
+                    .lvl(Level::Error(t.span.clone()))
+                    .msg(msg)
+                    .note(note)
+                    .build();
+                Err(diag)
+            }
+            Err(mut diag) => {
+                let note = format!("Expected the following token: `{}` at this point", expected,);
+                diag.add_note(note);
+                Err(diag)
+            }
+        }
+    }
+
+    fn peek(&self) -> PResult<&Token> {
+        self.peek_n(1)
+    }
+
+    fn peek_n(&self, n: usize) -> PResult<&Token> {
+        let n = n.saturating_sub(1);
+        let token = self.buf.get(self.cursor + n);
+        match token {
+            Some(t) => Ok(t),
+            None => {
+                let sp = self.get_last_span()?;
+                let diag = Diagnostic::builder()
+                    .lvl(Level::Error(sp))
+                    .msg("Unexpected EOF")
+                    .note("tried to peek too far into the future")
+                    .build();
+                Err(diag)
+            }
+        }
+    }
+
+    fn next_t(&mut self) -> PResult<&Token> {
+        let cursor = self.cursor;
+        self.cursor += 1;
+        match self.buf.get(cursor) {
+            Some(t) => Ok(t),
+            None => {
+                let sp = self.get_last_span()?;
+                let diag = Diagnostic::builder()
+                    .lvl(Level::Error(sp))
+                    .msg("Unexpected EOF")
+                    .build();
+                Err(diag)
+            }
+        }
+    }
+
+    fn get_last_span(&self) -> PResult<Span> {
+        match self.buf.last() {
+            Some(t) => Ok(t.span.clone()),
+            None => {
+                let diag = Diagnostic::builder()
+                    .lvl(Level::Internal(None))
+                    .msg("Parser :: failed to get last span")
+                    .note("Did you try to parse an empty file?")
+                    .build();
+                Err(diag)
+            }
+        }
+    }
+}
